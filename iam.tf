520334b3ecfe63906281ba0ab9f552f35e5aae47
aleksandrandreichenko <andreichenko@web.de>
2021-12-08 21:54:40 +0300

    added resources and provider

--- iam.tf
+++ iam.tf
@@ -1,3 +1,28 @@
+# Creating IAM role for Kubernetes clusters to make calls to other AWS services 
+# on our behalf to manage the resources that we use with the service.
+resource "aws_iam_role" "iam-role-eks-cluster" {
+  name = "terraformekscluster"
+  assume_role_policy = <<POLICY
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+   "Effect": "Allow",
+   "Principal": {
+    "Service": "eks.amazonaws.com"
+   },
+   "Action": "sts:AssumeRole"
+   }
+  ]
+ }
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks-cluster-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = "${aws_iam_role.iam-role-eks-cluster.name}"
+}
+
 # Creating IAM role for EKS nodes to work with other AWS Services. 
 
 resource "aws_iam_role" "eks_nodes" {
