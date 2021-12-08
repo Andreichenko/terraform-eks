a18d19748e645f9acde3661545887762aba61afd
aleksandrandreichenko <andreichenko@web.de>
2021-12-08 23:12:41 +0300

    added resources and provider

--- iam.tf
+++ iam.tf
@@ -43,3 +43,20 @@ resource "aws_iam_role" "eks_nodes" {
 }
 POLICY
 }
+
+# Attaching the different Policies to Node Members.
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_nodes.name
+}
--- main.tf
+++ main.tf
@@ -1,28 +1,3 @@
-# Creating IAM role for Kubernetes clusters to make calls to other AWS services 
-# on our behalf to manage the resources that we use with the service.
-resource "aws_iam_role" "iam-role-eks-cluster" {
-  name = "terraformekscluster"
-  assume_role_policy = <<POLICY
-{
- "Version": "2012-10-17",
- "Statement": [
-   {
-   "Effect": "Allow",
-   "Principal": {
-    "Service": "eks.amazonaws.com"
-   },
-   "Action": "sts:AssumeRole"
-   }
-  ]
- }
-POLICY
-}
-
-resource "aws_iam_role_policy_attachment" "eks-cluster-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = "${aws_iam_role.iam-role-eks-cluster.name}"
-}
-
 # Creating the EKS cluster
 
 resource "aws_eks_cluster" "eks_cluster" {
