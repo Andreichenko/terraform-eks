662e5fa2cc5f99a41db076142d62d3ee84651e1c
aleksandrandreichenko <andreichenko@web.de>
2021-12-08 21:54:25 +0300

    added resources and provider

---
+++ iam.tf
@@ -0,0 +1,20 @@
+# Creating IAM role for EKS nodes to work with other AWS Services. 
+
+resource "aws_iam_role" "eks_nodes" {
+  name = "eks-node-group"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
