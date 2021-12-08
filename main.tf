3b975754e60bfd79f1c1f9f7d18ca143943dc045
aleksandrandreichenko <andreichenko@web.de>
2021-12-08 23:42:54 +0300

    added resources and provider

--- main.tf
+++
@@ -1,6 +0,0 @@
-# Creating the EKS cluster
-
-resource "aws_eks_cluster" "eks_cluster" {
-  name     = "terraformEKScluster"
-  role_arn =  "${aws_iam_role.iam-role-eks-cluster.arn}"
-  version  = "1.23"
