9e7227a556ba726f95528afc09b3fb55cea430cd
aleksandrandreichenko <andreichenko@web.de>
2021-12-08 17:07:08 +0300

    added resources and provider

--- main.tf
+++ main.tf
@@ -22,3 +22,21 @@ resource "aws_iam_role_policy_attachment" "eks-cluster-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = "${aws_iam_role.iam-role-eks-cluster.name}"
 }
+
+resource "aws_vpc" "vpc-east-1" {
+  provider             = aws.region-common
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name        = "common-vpc-eks"
+    Owner       = "Aleksandr Andreichenko"
+    Environment = "Production Environment"
+    Region      = "eu-central-1"
+  }
+}
+
+resource "aws_security_group" "eks-cluster" {
+  name        = "SG-eks-cluster"
+  vpc_id      = aws_vpc.vpc-east-1.id
+  } 
--- variables.tf
+++ variables.tf
@@ -9,3 +9,15 @@ variable "region-common" {
   default     = "eu-east-1"
   description = "The common region"
 }
+
+variable "vpc_name" {
+  description = "The name of the common VPC"
+  type        = string
+  default     = "The common VPC"
+}
+
+variable "vpc_cidr_block" {
+  description = "The IPv4 CIDR block of the common VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
