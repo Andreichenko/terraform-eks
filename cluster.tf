a36226692ed3feb4a412f421d291a74bddfe9906
aleksandrandreichenko <andreichenko@web.de>
2021-12-09 00:28:57 +0300

    added resources and provider

--- cluster.tf
+++ cluster.tf
@@ -5,3 +5,22 @@ locals {
 module "vpc" {
 
 }
+
+module "eks" {
+  source       = "git::https://github.com/terraform-aws-modules/terraform-aws-eks.git?ref=v12.1.0"
+  cluster_name = local.cluster_name
+  vpc_id       = module.vpc.aws_vpc_id
+  subnets      = module.vpc.aws_subnet_private_prod_ids
+  
+  node_groups = {
+    eks_nodes = {
+      desired_capacity = 3
+      max_capacity     = 3
+      min_capaicty     = 3
+
+      instance_type = "t2.small"
+    }
+  }
+
+  manage_aws_auth = false
+}
