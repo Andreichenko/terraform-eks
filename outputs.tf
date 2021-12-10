28d8ec59a92a163a8f9cd8aeb0187221c3a2d0e2
aleksandrandreichenko <andreichenko@web.de>
2021-12-10 22:56:47 +0300

    changes input variables

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cluster_arn" {
-  description = "The Amazon Resource Name (ARN) of the cluster."
-  value       = local.cluster_arn
-}
-
-output "cluster_version" {
-  description = "The Kubernetes server version for the EKS cluster."
-  value       = element(concat(aws_eks_cluster.this[*].version, [""]), 0)
-}
