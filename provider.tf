c619b20c12d4da7c3f659318f4d30e5587f60569
aleksandrandreichenko <andreichenko@web.de>
2021-12-10 22:58:00 +0300

    changes input variables

--- provider.tf
+++ provider.tf
@@ -1,11 +1,4 @@
 provider "aws" {
   version = "~> 2.57.0"
   region  = "us-east-1"
-
- backend "s3" {
-    region  = "us-east-1"
-    profile = "default"
-    key     = "terraform-state-file/statefile.tfstate"
-    bucket  = "terraform-state-cname-bucket-9"
-  }
 }
